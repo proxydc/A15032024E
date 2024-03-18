3741c0b1a21bb47a919b486c2c78410aef4dd937
proxydc <alexproxiad@gmail.com>
2024-03-18 10:07:59 +0100

    .env

--- server/server.js
+++ server/server.js
@@ -3,25 +3,24 @@ const dbRoutes = require('./src/database/routes');
 const app = express();
 const PORT = 3000;
 const cors = require('cors');
-const siteUrlDev="http://localhost:8080";
-const siteUrlProd="https://mysitevue.onrender.com";
+const siteUrlDev = process.env.VUE_APP_SITEURLDEV; //"http://localhost:8080";
+const siteUrlProd = process.env.VUE_APP_SITEURLPROD; //"https://mysitevue.onrender.com";
 
 app.use(express.json());
 app.use(cors({
-    origin:  app.get("env") == "development"?
-    siteUrlDev: siteUrlProd,
+    origin: app.get("env") == "development" ?
+        siteUrlDev : siteUrlProd,
 }));
-app.get("/", (req, res)=> {
+app.get("/", (req, res) => {
     res.send("Hello Proxiad Backend!");
 });
 app.use("/api/v1/database", dbRoutes);
-app.listen(PORT, function check(err)
-{
-    console.log("devurl: "+ siteUrlDev);
-    console.log("produrl: "+ siteUrlProd);
-    console.log("env: "+ app.get("env"));
-    if(err)
-    console.log("error");
+app.listen(PORT, function check(err) {
+    console.log("devurl: " + siteUrlDev);
+    console.log("produrl: " + siteUrlProd);
+    console.log("env: " + app.get("env"));
+    if (err)
+        console.log("error");
     else
-    console.log("started on port "+ PORT);
+        console.log("started on port " + PORT);
 });
